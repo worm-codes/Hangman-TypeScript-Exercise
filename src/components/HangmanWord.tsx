54fa686a4cd8c2ee886e12cbd4011615aead2fdd
oguzhan.cevik <oguzhan@90pixel.com>
2022-11-18 16:51:18 +0300

    HangmanWords Crated

--- src/components/HangmanWord.tsx
+++ src/components/HangmanWord.tsx
@@ -1,9 +1,31 @@
-
+import styled from "styled-components"
 
 const HangmanWord = () => {
+    const word='test'
+    const guessedLetters=['t','e']
   return (
-    <div>HangmanWord</div>
+    <WordContainer>
+     
+        {word.split('').map((letter,index)=>( <UnderLine key={index}>
+        <Letter correct={guessedLetters.includes(letter)}>{letter}</Letter>
+      </UnderLine>))}
+    </WordContainer>
   )
 }
 
 export default HangmanWord
+
+const WordContainer=styled.div`
+    display: flex;
+    gap: .25em;
+    font-size: 6rem;
+    font-weight: bold;
+    text-transform: uppercase;
+    font-family: monospace;
+`
+const UnderLine=styled.div`
+  border-bottom: 0.1em solid black;
+`
+const Letter=styled.div`
+  visibility: ${({correct})=>correct?'visible':'hidden'};;
+`
